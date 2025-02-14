5c819b4b002e0ce8ddd70b295ab4615491e659e8
Maurice Drew <drewm1987@gmail.com>
2025-02-14 15:56:26 -0600

    added css

---
+++ javascript.js
@@ -0,0 +1,10 @@
+// github commands offline
+// 1. call a console.log wthe the string "Hello Worldl" 
+// 2. load hello world on each page on home, about, work/portfolio
+// 3. load hellot world on about page
+// 4. load hello world on work/portfolio
+// 5. Test page
+
+
+const greeting ="Hello World!"
+console.log (greeting)
